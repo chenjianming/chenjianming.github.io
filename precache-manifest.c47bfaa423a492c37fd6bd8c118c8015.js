f7e680adc4161b92442032916094f3fcfb291d80
rico <ming61177@gmail.com>
2019-04-09 14:58:33 +0800

    english blog

---
+++ precache-manifest.c47bfaa423a492c37fd6bd8c118c8015.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "175e5e63814a6271eb01",
+    "url": "/static/js/main.6f466131.chunk.js"
+  },
+  {
+    "revision": "5ed8a0c95e8bbdad7958",
+    "url": "/static/js/2.ece8c017.chunk.js"
+  },
+  {
+    "revision": "175e5e63814a6271eb01",
+    "url": "/static/css/main.4a08f8a5.chunk.css"
+  },
+  {
+    "revision": "c31742206606b49ab167a3ffeb652a7a",
+    "url": "/index.html"
+  }
+];
